4aceed277d294156c9c4e37ac97dcc2cfd3b4f97
hillarymuir <hillarymuir@pm.me>
2025-12-12 11:39:33 -0500

    set to 60 fps; create circleshape file

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -18,6 +21,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        dt = (clock.tick(60)/1000)
+        print(f"{dt}")
 
 
 if __name__ == "__main__":
